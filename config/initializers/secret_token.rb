a6920dbd6ecf9c486194804442ebf6a453f765c0
Bruna Vasques <brunalrpv@gmail.com>
2011-01-09 17:35:39 -0200

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoStore::Application.config.secret_token = '8ad053a43b0dc324c07794aa30109262da2b56da5c2cf4a134b61c1439e4b6d128444438b5f04d4e5b5c19c6d9b2fca9f02a085948a2626c401ab7fb58f52154'
