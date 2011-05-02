384f14cf18bce418457f15fcc0627b328ff3b5a1
Bruna Vasques <brunalrpv@gmail.com>
2011-05-02 09:47:00 -0300

    Not allowing to add item twice to cart

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -6,11 +6,20 @@ module VideoHelper
         videos = orders.map do |order|
           order.products
         end
-        if videos.include? [video]
+        if (videos.include? [video]) || (videos.first.include? video)
           return true
         end
       end
     end
     false
+  end
+
+  def already_on_cart?(video)
+    if current_order
+      if current_order.products.include? video
+        return true
+      end
+      false
+    end
   end
 end
